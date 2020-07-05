5aeecef205f0dc532f8f024dee6964af16937ecf
Kwaku Ankobia <email@example.com>
2020-07-05 07:05:56 +0100

    feat(mogoDB): add connect mongoDB

--- contact-keeper/server.js
+++ contact-keeper/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+connectDB();
 
 // define routes 
 app.use('/api/users', require('./routes/users'));
@@ -9,5 +10,6 @@ app.use('/api/contacts', require('./routes/contacts'));
 app.use('/api/auth', require('./routes/auth'));
 
 app.get('/', (req,res) => res.json({msg: 'Hello World'}));
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started at ${PORT}`));
